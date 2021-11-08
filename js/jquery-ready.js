0209fde1b7a61c46a97bed7f41e292324ee1ee88
Vladislav <vzv@siluetstudio.com>
2021-11-08 15:54:33 +0300

    projects page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -80,7 +80,55 @@ $(document).ready(function() {
     if ($('.license').length) {
         $('.license__item .name').matchHeight();
     }
-    
+
+    if ($('.project').length) {
+        $('.project__item .name').matchHeight();
+    }    
     //=======одинаковая высота текстовых блоков КОНЕЦ========
+
+    //=======попап в проектах=========
+    if ($('.project').length) {
+        $('.project__item').click(function () {
+            let currentProject = $(this);
+            let currentProjectColor = $(this).find('.content').css('background-color');
+            let currentImgHref = currentProject.find('.image img').attr('src');
+            let currentAnotherImgHref = currentProject.attr('data-img');
+            console.log(Boolean(currentAnotherImgHref))
+            let currentName = currentProject.find('.content .name');
+            let currentInfo = currentProject.find('.content .info');
+            let currentWysiwyg = currentProject.find('.hidden .wysiwyg');
+
+            let popup = $('.project-popup');
+            let popupContent = popup.find('.project-popup__wrap .content');
+            popup.find('.image img').attr('src', '');
+            popupContent.html('');
+            popupContent.css('background-color', currentProjectColor)
+
+            if (Boolean(currentAnotherImgHref) == false) {
+                popup.find('.image img').attr('src', currentImgHref);
+            } else {
+                popup.find('.image img').attr('src', currentAnotherImgHref);
+            }
+            
+
+            let popupContentName = currentName.clone();
+            let popupContentInfo = currentInfo.clone();
+            let popupContentWysiwyg = currentWysiwyg.clone();
+
+            popupContent.append(popupContentName);
+            popupContent.append(popupContentInfo);
+            popupContent.append(popupContentWysiwyg);
+
+            popup.fadeIn();
+
+            $('body').addClass('no-scroll');
+        })
+
+        $('.project-popup').on('click', '.close', function() {
+            $(this).closest('.project-popup').fadeOut();
+            $('body').removeClass('no-scroll');
+        })
+    }
+    //=======попап в проектах КОНЕЦ=========
 
 });
